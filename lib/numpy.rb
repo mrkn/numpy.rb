19e9e12aabd09e9ae4c0b88ab7341c69cfa40528
Kenta Murata <mrkn@mrkn.jp>
2017-09-08 15:26:42 +0900

    Wrap some classes

--- lib/numpy.rb
+++ lib/numpy.rb
@@ -1,5 +1,38 @@
 require "numpy/version"
+require "pycall"
 
+np = PyCall.import_module('numpy')
+np::VERSION = Numpy::VERSION
+Object.send :remove_const, :Numpy
+
+Numpy = np
 module Numpy
-  # Your code goes here...
+  CharArray = self.chararray
+  CharArray.__send__ :register_python_type_mapping
+
+  Memmap = self.memmap
+  Memmap.__send__ :register_python_type_mapping
+
+  Matrix = self.matrix
+  Matrix.__send__ :register_python_type_mapping
+
+  Poly1D = self.poly1d
+  Poly1D.__send__ :register_python_type_mapping
+
+  MA = self.ma
+  module MA
+    MaskedArray = self.MaskedArray
+  end
+
+  Polynomial = self.polynomial
+  module Polynomial
+    Chebyshev = self.chebyshev.Chebyshev
+    Hermite = self.hermite.Hermite
+    HermiteE = self.hermite_e.HermiteE
+    Laguerre = self.laguerre.Laguerre
+    Legendre = self.legendre.Legendre
+    Polynomial = self.polynomial.Polynomial
+  end
 end
+
+require 'numpy/ndarray'
--- spec/numpy_spec.rb
+++ spec/numpy_spec.rb
@@ -5,7 +5,24 @@ RSpec.describe Numpy do
     expect(Numpy::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  specify do
+    expect(Numpy.asarray([1])).to be_a(Numpy::NDArray)
+  end
+
+  specify 'scalar types conversion' do
+    expect(Numpy.asarray([1])[0]).to be_a(Integer)
+    expect(Numpy.asarray([1.3])[0]).to be_a(Float)
+    expect(Numpy.asarray([1.3 + 1i])[0]).to be_a(Complex)
+    expect(Numpy.asarray([true])[0]).to be_a(TrueClass)
+    expect(Numpy.asarray([false])[0]).to be_a(FalseClass)
+  end
+
+  describe '.asscalar' do
+    specify do
+      expect(Numpy.asscalar(Numpy.asarray([1]))).to be_a(Integer)
+      expect(Numpy.asscalar(Numpy.asarray([1.3]))).to be_a(Float)
+      expect(Numpy.asscalar(Numpy.asarray([1.3 + 1i]))).to be_a(Complex)
+      expect(Numpy.asscalar(Numpy.asarray([true]))).to be_a(TrueClass)
+    end
   end
 end
