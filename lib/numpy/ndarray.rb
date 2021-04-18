eeee8b395c696f73b9bf65cb2f760fe52bca422e
Kenta Murata <mrkn@mrkn.jp>
2021-04-18 11:15:40 +0900

    Add NDArray#to_a

--- lib/numpy/ndarray.rb
+++ lib/numpy/ndarray.rb
@@ -6,5 +6,20 @@ module Numpy
     def [](*index)
       Conversion.to_ruby(super)
     end
+
+    def to_a
+      recursive_to_a(tolist)
+    end
+
+    private def recursive_to_a(list)
+      list.to_a.collect! {|l|
+        case l
+        when PyCall::List
+          recursive_to_a(l)
+        else
+          l
+        end
+      }
+    end
   end
 end
